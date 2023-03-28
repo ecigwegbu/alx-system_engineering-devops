b6c3e8419da3d377779eec860fad759bf0c74cbf
ecigwegbu <igwegbu@gmail.com>
2023-03-28 04:44:01 +0100

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This puuppet manifest creates a file in /tmp
+file { 'create_a_file':
+  path => '/tmp/school'
+  mode => '0744';
+  owner => www-data;
+  group => www-data;
+  content => 'I love Puppet'
+}
