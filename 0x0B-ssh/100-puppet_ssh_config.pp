fea2c70444148a2a31bdd5be6ea0302633585857
ecigwegbu <igwegbu@gmail.com>
2023-03-31 04:11:07 +0100

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,10 @@
 #ssh client configuration using puppet
 
-$src = "#ssh client configuration using puppet
-PasswordAuthentication no
-IdentityFile ~/.ssh/school
-"
-file { '/etc/ssh/ssh_config.d/client.conf':
-  path    => '/etc/ssh/ssh_config.d/client.conf',
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => $src,
+exec { 'edit_ssh_config':
+  command => "/usr/bin/echo '    IdentityFile ~/.ssh/school' >> '/etc/ssh/ssh_config';
+              /usr/bin/echo '    PasswordAuthentication no' >> '/etc/ssh/ssh_config' ",
   notify  => Service['ssh']
 }
-
 service { 'ssh':
   ensure => running,
   enable => true
