a3e9961464c38a3f7a38e0c6f896068acb5e4525
ecigwegbu <igwegbu@gmail.com>
2023-04-10 05:59:13 +0100

    . Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/test.pp
+++ 0x0C-web_server/test.pp
@@ -1,6 +1,3 @@
-# Puppet Manifest to install nginx
-# install nginx on a new ubuntu server
-
 #apt update
 exec { 'apt_update':
   command => '/usr/bin/apt update',
@@ -11,12 +8,12 @@ exec { 'apt_update':
 package { 'nginx':
   ensure   => installed,
   provider => 'apt',
-  notify   => File['/var/www/html/index.html'],
+  notify   => File['/var/www/html/index2.html'],
 }
 
 # Hello World file
-file { '/var/www/html/index.html':
-  path    => '/var/www/html/index.html',
+file { '/var/www/html/index.html2':
+  path    => '/var/www/html/index2.html',
   mode    => '0644',
   content => 'Hello World!',
   notify  => Service['nginx'],
@@ -28,66 +25,48 @@ service { 'nginx':
   enable  => true,
   require => Package['nginx'],
 }
+
 # apt install -y ufw
 package { 'ufw':
   ensure   => installed,
   provider => 'apt',
-}
+} ->
 
 # ufw incoming
 exec { 'ufw_def_incoming':
   command => '/usr/sbin/ufw default deny incoming',
-}
+} ->
 # ufw outgoing
 exec { 'ufw_def_outgoing':
   command => '/usr/sbin/ufw default allow outgoing',
-}
+} ->
 
-# ufw port 22 
+# ufw port 22
 exec { 'ufw_port22':
   command => '/usr/sbin/ufw allow 22/tcp',
-}
-# ufw port 80 
+} ->
+# ufw port 80
 exec { 'ufw_port80':
   command => '/usr/sbin/ufw allow 80/tcp',
-}
-# ufw enable 
+} ->
+# ufw enable
 exec { 'ufw_enable':
   command => '/usr/sbin/ufw --force enable',
 }
 
 # First, remove any previous redirect:
 exec { 'sed_rem_prev_redirect':
-  command => '/usr/bin/sed -i --follow-symlinks "s/^\s*rewrite \/redirect_me.*$//g" \
-    /etc/nginx/sites-enabled/default',
+  command => '/usr/bin/sed -i --follow-symlinks \
+    "s/^\s*rewrite \/redirect_me.*$//g" default',
 }
 
 # Redirection - 301
 $rline1="\tserver_name _;
-	rewrite /redirect_me"
+        rewrite /redirect_me"
 $newurl='https://www.youtube.com/watch?v=QH2-TGUlwu4'
 exec { 'sed_redirection_301':
   command => '/usr/bin/sed -i --follow-symlinks \
     "s/^[[:blank:]]server_name _;/$rline1 $newurl permanent;/" \
-    /etc/nginx/sites-enabled/default',
+    default',
   require => Exec['sed_rem_prev_redirect'],
-}
-
-# Not found page 404
-file { '/usr/share/nginx/html/error404.html':
-  path    => '/usr/share/nginx/html/error404.html',
-  mode    => '0644',
-  content => "Ceci n'est pas une page",
-  notify  => Exec['404_not_found'],
-}
-$newline="\tserver_name _;
-	error_page 404 /error404.html;
-	location = /error404.html {
-		root /usr/share/nginx/html;
-		internal;
-		}"
-exec { '404_not_found':
-  command => '/usr/bin/sed -i "s/^[[:blank:]]server_name _;/$newline/" \
-    /etc/nginx/sites-enabled/default',
-  notify  => Service['nginx'],
 }
