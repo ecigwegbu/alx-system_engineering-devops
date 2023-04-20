59e7f4b4088517a8d3992f93d0337c6a4d4498c8
ecigwegbu <igwegbu@gmail.com>
2023-04-20 08:04:30 +0100

    5. MySQL backup

--- 0x14-mysql/0-install-mysql5.7.sh
+++ 0x14-mysql/0-install-mysql5.7.sh
@@ -12,7 +12,35 @@ sudo sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >>
 	/etc/apt/sources.list.d/mysql.list'
 
 sudo apt-get update
+sudo apt upgrade -y
 sudo apt-cache policy mysql-server
 sudo apt install -y -f mysql-client=5.7* mysql-community-server=5.7* \
 	mysql-server=5.7*
+sudo mysql --version
 
+sudo apt install ufw -y
+sudo ufw default incoming deny
+sudo ufw default outgoing allow
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 8080
+sudo ufw allow 3306
+sudo ufw --force enable
+sudo systemctl status ufw
+sudo sustemctl enable --now mysql
+#source ~/bin/mysql-config.sh
+#source ~/bin/nginx-install-config.sh
+#source install others:
+#  git
+#  pycodestyle
+#  betty
+#  shellcheck
+#  puppet
+#  puppet-lint
+#  semistandard
+#  vim
+#  nodejs
+#  fab?
+#  python3
+#  aliases
