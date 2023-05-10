b7914ba9c108fd9c395c1854f378c6f220fde635
ecigwegbu <igwegbu@gmail.com>
2023-05-11 00:43:29 +0100

    module doc update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-''' Read data from the reddit API. '''
+""" Read data from the reddit API. """
 import json
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    ''' Queries the Reddit API and returns the number of subscribers '''
+def recurse(subreddit, hot_list=[], after=""):
+    """ Queries the Reddit API and returns the number of subscribers """
 
     # Build the query string
-    if after == '':
-        AFTER = ''
+    if after == "":
+        AFTER = ""
     else:
-        AFTER = '?after={}'.format(after)
+        AFTER = "?after={}".format(after)
 
     # build page list
-    headers = {'User-Agent': 'alx2-web-app'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json{AFTER}'
+    headers = {"User-Agent": "alx2-web-app"}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json{AFTER}"
     res = requests.get(url, headers=headers, allow_redirects=False)
     page_list = []
     if res.status_code != 200:
         return None
-    if 'children' in res.json()['data']:
-        for child in res.json()['data']['children']:
-            page_list.append(child['data']['title'])
+    if "children" in res.json()["data"]:
+        for child in res.json()["data"]["children"]:
+            page_list.append(child["data"]["title"])
 
     hot_list += page_list
-    if 'after' not in res.json()['data']:
+    if "after" not in res.json()["data"]:
         return hot_list
-    after = res.json()['data']['after']
+    after = res.json()["data"]["after"]
     if after is None:
         return hot_list
 
