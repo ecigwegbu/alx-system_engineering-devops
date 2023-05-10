dfccbdb9d7e7dfa5b08578f360ec44ce3ced2586
ecigwegbu <igwegbu@gmail.com>
2023-05-11 00:24:44 +0100

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit.
-
-Requirements:
-
-Prototype: def top_ten(subreddit)
-If not a valid subreddit, print None.
-NOTE: Invalid subreddits may return a redirect to search results. Ensure that
-you are not following redirects
-"""
-
-import Requests
+''' Read data from the reddit API. '''
+import json
+import requests
 
 
-def number_of_subscribers(subreddit):
-    """Read data from reddit API"""
-    pass
+def recurse(subreddit, hot_list=[], after=''):
+    ''' Queries the Reddit API and returns the number of subscribers '''
+
+    # Build the query string
+    if after == '':
+        AFTER = ''
+    else:
+        AFTER = '?after={}'.format(after)
+
+    # build page list
+    headers = {'User-Agent': 'alx2-web-app'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json{AFTER}'
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    page_list = []
+    if res.status_code != 200:
+        return None
+    if 'children' in res.json()['data']:
+        for child in res.json()['data']['children']:
+            page_list.append(child['data']['title'])
+
+    hot_list += page_list
+    if 'after' not in res.json()['data']:
+        return hot_list
+    after = res.json()['data']['after']
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after)
