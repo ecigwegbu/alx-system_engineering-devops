c3c4ea8eab864827608d4ab739de8e05003575f9
ecigwegbu <igwegbu@gmail.com>
2023-05-10 13:42:54 +0100

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
-"""
-Read data from the reddit API. The programme will check
-for the presence of the keyword programming
-"""
-
+""" Read data from the reddit API. """
 import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Write a function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the function should return 0
-    """
-    myID = 'ajjYdzvjEiJSfunPCWQ6kg'
-    myKey = '8iKEr-e6aowo2Dl8gJmvntRDPwIflw'
-    auth = requests.auth.HTTPBasicAuth(myID, myKey)
-    data = {
-            'grant_type': 'password',
-            'username': 'igwegbu',
-            'password': 'FJS-m932S-BWu.8'
-           }
-    headers = {'User-Agent': 'alx-api-rdt'}
+    """ Queries the Reddit API and returns the number of subscribers """
+    headers = {'User-Agent': 'alx2-web-app'}
 
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
-    token = res.json()['access_token']
-    headers['authorization'] = f'bearer {token}'
-
-    res = requests.get('https://oauth.reddit.com/api/subreddit_autocomplete?' +
-                       'query=' + subreddit, headers=headers)
-    # print(res.status_code, res.status_code == 200)
-    if res.status_code == 404:
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    res = requests.get(url, headers=headers)
+    if res.status_code != 200:  # search item not found?
         return 0
-    res2 = res.json()
-    res_dict = res2['subreddits']
-    total = 0
-    for subreddit in res_dict:
-        total += subreddit['numSubscribers']
-    return total
+
+    res_json = res.json()  # get json output
+    res_data = res_json['data']  # the required dictionary is in 'data' key
+    if 'subscribers' in res_data:
+        num_subscribers = res_data['subscribers']  # .. in 'subscribers' field
+    else:
+        num_subscribers = 0  # no subscribers
+    return num_subscribers
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Search argument required")
+        exit()
+    print(number_of_subscribers(sys.argv[1]))
