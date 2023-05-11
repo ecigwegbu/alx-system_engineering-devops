e1c51e527cc93b93406d77a7fff0a963f7f7179b
ecigwegbu <igwegbu@gmail.com>
2023-05-11 01:10:52 +0100

    module doc

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-""" Read data from the reddit API. """
+""" Write a function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0
+"""
 import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Queries the Reddit API and returns the number of subscribers """
+    """ Queries the Reddit API and returns the number of subscribers
+        Args:
+            subreddit (str)
+    """
     headers = {"User-Agent": "alx2-web-app"}
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
