a39c022a77584a252c856f6f76182e65374dd4c3
ecigwegbu <igwegbu@gmail.com>
2023-05-12 17:47:02 +0100

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,7 @@
 # puppet manifest to find out why Apache is returning a 500 error.
-# First rename the index.php that is causing wahalla :)
-file { '/var/www/html/index.php':
-  ensure  => absent
-}
 
-# Then add a placeholder while I figure out what is wrong! :)
-file { '/var/www/html/index.html':
-  ensure  => file,
-  mode    => '0755',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => '<html><body>#Do.Hard.Things! :)</body></html>'
+# replace bad file extension phpp with php:
+exec { 'sed_repl_phpp':
+  command => '/bin/sed -i --follow-symlinks "s/phpp/php/g" \
+    /var/www/html/wp-settings.php',
 }
