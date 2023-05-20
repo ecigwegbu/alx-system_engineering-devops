6d7d1ec1cb10095143dedfc38fa82e4698a6e6bb
ecigwegbu <igwegbu@gmail.com>
2023-05-20 21:30:24 +0100

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,6 +3,8 @@
 # Determine dthat 256 is good for up to 2000 requests @ 100 concur.
 # ab -c 10 -n 2000 localhost/ | grep 'Failed requests:'
 
-exec { 'increase_ULIMIT':
-  command => '/bin/sed -i "s/15/256/g" nginx',
+# ULIMIT="-n 15"
+file { '/root/nginx':
+  ensure  => file,
+  content => 'ULIMIT="-n 256"',
 }
