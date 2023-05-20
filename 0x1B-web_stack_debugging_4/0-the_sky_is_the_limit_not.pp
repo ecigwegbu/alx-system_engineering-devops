2bd212437aee0e697131d0660cea30ce13eb1a67
ecigwegbu <igwegbu@gmail.com>
2023-05-20 22:30:05 +0100

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Set ULIMIT to make 2000 requests to my server with 100 requests at a time
 
-exec { "update_ulimit":
+exec { 'update_ulimit':
   command => '/bin/sed -i "s/n 15/n 2048/g" /etc/default/nginx',
 }
