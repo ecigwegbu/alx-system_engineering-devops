4d9392b236ae4fe1fa79cb21b2451e8968eef8c3
ecigwegbu <igwegbu@gmail.com>
2023-05-21 11:27:49 +0100

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,3 +5,8 @@ exec { 'update_ulimit':
   command => '/bin/sed -i -E "s/^(ULIMIT=.*)$/ULIMIT=\"-n 256\"/g" \
   /etc/default/nginx',
 }
+
+# Reload the nginx configuration
+-> exec { 'reload_nginx':
+  command => '/usr/sbin/service nginx reload',
+}
